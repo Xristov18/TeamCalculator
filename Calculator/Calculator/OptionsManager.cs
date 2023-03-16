54c3f121805cf6505acd19bede46c465258e4f37
pmgvspernik@gmail.com <pmgvspernik@gmail.com>
2023-03-16 08:55:03 +0200

    0

--- Calculator/Calculator/OptionsManager.cs
+++ Calculator/Calculator/OptionsManager.cs
@@ -38,14 +38,11 @@ namespace Calculator
         {
             return $"{a} - {b} = {a - b}";
         }
-
-
-
-        
         public static string Multiply(double a, double b)
         {
             return $"{a} * {b} = {a * b}";
 
+        }
         public static string Devide(double a,double b)
         {
             if (b==0)
@@ -57,7 +54,5 @@ namespace Calculator
                 return $"{a} / {b} = {a / b}";
             }
         }
-        
-        
     }
 }
